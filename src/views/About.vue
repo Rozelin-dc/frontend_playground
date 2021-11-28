f2fb44439af4ed60c07bfbc665b24d85ccd43cf2
Rozelin <rozelin.dc@gmail.com>
2021-11-28 22:37:26 +0900

    install two.js-ts

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div id="playground" />
   </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Two from "twojs-ts";
+
+@Options({
+  name: "About",
+})
+export default class Home extends Vue {
+  element: HTMLElement | null = null;
+  two: Two | null = null;
+  circle: Two.Circle | null = null;
+
+  mounted(): void {
+    this.element = document.getElementById("#playground");
+
+    if (!this.element) {
+      return;
+    }
+
+    this.two = new Two().appendTo(this.element);
+    this.circle = this.two.makeCircle(0, 0, 50);
+    this.circle.fill = "#FF8000";
+  }
+}
+</script>
